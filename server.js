aa4c7400d03a8fcf4535f825337b52e28786d5cb
Jyoti Sinha <jyotisinha1502@gmail.com>
2024-05-27 00:15:56 +0530

    env files

--- server.js
+++ server.js
@@ -39,11 +39,10 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 
-require["dotenv"].config();
+require("dotenv").config();
 
 const bodyparser = require("body-parser");
 app.use(bodyparser.json()); // req.body
-
 
 const PORT = process.env.PORT || 3000;
 
